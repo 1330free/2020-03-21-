db81cc0462e9e78ae87975015b795f165e84faf9
1330free <92074698+1330free@users.noreply.github.com>
2022-04-12 22:00:25 +0800

    Add files via upload

---
+++ 10944123/asset/jsp/a.js
@@ -0,0 +1,99 @@
+function b() {
+    document.getElementById("A").style.display = "none"
+    document.getElementById("B").style.display = "block"
+}
+
+function b1() {
+    document.getElementById("B").style.display = "none"
+    document.getElementById("A").style.display = "block"
+}
+
+function d() {
+    document.getElementById("D").style.display = "none"
+    document.getElementById("E").style.display = "block"
+}
+
+function d1() {
+    document.getElementById("E").style.display = "none"
+    document.getElementById("D").style.display = "block"
+}
+
+function e() {
+    document.getElementById("F").style.display = "none"
+    document.getElementById("G").style.display = "block"
+}
+
+function e1() {
+    document.getElementById("G").style.display = "none"
+    document.getElementById("F").style.display = "block"
+}
+
+function f() {
+    document.getElementById("H").style.display = "none"
+    document.getElementById("I").style.display = "block"
+}
+
+function f1() {
+    document.getElementById("I").style.display = "none"
+    document.getElementById("H").style.display = "block"
+}
+
+function g() {
+    document.getElementById("J").style.display = "none"
+    document.getElementById("K").style.display = "block"
+}
+
+function g1() {
+    document.getElementById("K").style.display = "none"
+    document.getElementById("J").style.display = "block"
+}
+
+function h() {
+    document.getElementById("M").style.display = "none"
+    document.getElementById("N").style.display = "block"
+}
+
+function h1() {
+    document.getElementById("N").style.display = "none"
+    document.getElementById("M").style.display = "block"
+}
+
+function i() {
+    document.getElementById("S").style.display = "none"
+    document.getElementById("T").style.display = "block"
+}
+
+function i1() {
+    document.getElementById("T").style.display = "none"
+    document.getElementById("S").style.display = "block"
+}
+
+function j() {
+    document.getElementById("U").style.display = "none"
+    document.getElementById("V").style.display = "block"
+}
+
+function j1() {
+    document.getElementById("V").style.display = "none"
+    document.getElementById("U").style.display = "block"
+}
+
+function k() {
+    document.getElementById("W").style.display = "none"
+    document.getElementById("X").style.display = "block"
+}
+
+function k1() {
+    document.getElementById("X").style.display = "none"
+    document.getElementById("W").style.display = "block"
+}
+
+function l() {
+    document.getElementById("Y").style.display = "none"
+    document.getElementById("Z").style.display = "block"
+}
+
+function l1() {
+    document.getElementById("Z").style.display = "none"
+    document.getElementById("Y").style.display = "block"
+}
